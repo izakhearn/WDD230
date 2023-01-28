b37dda9e97dfeedf3328f881bcb62022826419c3
Izak Hern <izakhearn@gmail.com>
2023-01-28 13:24:16 +1100

    Join.html

--- coc/scripts/dates.js
+++ coc/scripts/dates.js
@@ -4,3 +4,15 @@ var lastModified = document.lastModified;
 
 document.getElementById("copyYear").innerHTML = curYear;
 document.getElementById("lastModified").innerHTML = "Last Modified: "+lastModified;
+
+try {
+    var form = document.getElementById("join");
+
+    // Add event listener to form
+    form.addEventListener("submit", function (event) {
+        var time = new Date().toLocaleString();
+        document.getElementById("time").value = time;
+
+    });
+} catch (e) {
+}
